9731e663dc9e694dacc34f3da8c8503cd8c5b9d6
descawed <jacob.smallwood91@gmail.com>
2020-08-02 11:05:33 -0400

    Plugin documentation

--- tesutil/src/plugin.rs
+++ tesutil/src/plugin.rs
@@ -138,10 +138,7 @@ impl Plugin {
     ///
     /// # fn main() -> Result<(), PluginError> {
     /// let mut plugin = Plugin::new(String::from("test"), String::from("sample plugin"))?;
-    /// let mut record = Record::new(b"GMST");
-    /// record.add_field(Field::new_string(b"NAME", String::from("iDispKilling"))?);
-    /// record.add_field(Field::new_i32(b"INTV", -50));
-    /// plugin.add_record(record)?;
+    /// plugin.is_master = true;
     /// # Ok(())
     /// # }
     /// ```
@@ -165,6 +162,30 @@ impl Plugin {
         })
     }
 
+    /// Read a plugin file from the provided reader
+    ///
+    /// Reads a plugin from any type that implements [`Read`] or a mutable reference to such a type.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`std::io::Error`] if the format of the plugin data is invalid or if an I/O error
+    /// occurs while reading the plugin data.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use tesutil::plugin::*;
+    ///
+    /// # fn main() -> std::io::Result<()> {
+    /// let buf: Vec<u8> = vec![/* raw plugin data */];
+    /// let plugin = Plugin::read(&mut &buf[..])?;
+    /// println!("Plugin info: author {}, description {}", plugin.author(), plugin.description());
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`Read`]: https://doc.rust-lang.org/std/io/trait.Read.html
+    /// [`std::io::Error`]: https://doc.rust-lang.org/std/io/struct.Error.html
     pub fn read<T: Read>(mut f: T) -> io::Result<Plugin> {
         let header = Record::read(&mut f)?;
         if header.name() != b"TES3" {
@@ -237,39 +258,179 @@ impl Plugin {
         Ok(plugin)
     }
 
+    /// Reads a plugin from a file
+    ///
+    /// Reads a plugin from the file at `path`. The entire plugin is read into memory and retains
+    /// no reference to the file once the read is complete.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`std::io::Error`] if the file cannot be opened or if [`Plugin::read`] fails;
+    /// refer to that method for more information.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use tesutil::plugin::*;
+    /// # use std::io;
+    ///
+    /// # fn main() -> io::Result<()> {
+    /// let plugin = Plugin::load_file("Morrowind.esm")?;
+    /// assert!(plugin.is_master);
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`std::io::Error`]: https://doc.rust-lang.org/std/io/struct.Error.html
+    /// [`Plugin::read`]: #method.read
     pub fn load_file(path: &str) -> io::Result<Plugin> {
         let f = File::open(path)?;
         let mut reader = BufReader::new(f);
         Plugin::read(&mut reader)
     }
 
+    /// Returns the plugin file version
+    ///
+    /// This should always return one of the version constants ([`VERSION_1_2`] or [`VERSION_1_3`]),
+    /// but could return something else if a plugin file is read that contains an unusual version
+    /// value.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use tesutil::plugin::*;
+    ///
+    /// # fn main() -> Result<(), PluginError> {
+    /// let plugin = Plugin::new(String::from("test"), String::from("sample plugin"))?;
+    /// assert_eq!(plugin.version(), VERSION_1_3);
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`VERSION_1_2`]: constant.VERSION_1_2.html
+    /// [`VERSION_1_3`]: constant.VERSION_1_3.html
     pub fn version(&self) -> f32 {
         self.version
     }
 
+    /// Returns the plugin author string
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use tesutil::plugin::*;
+    /// # use std::io;
+    ///
+    /// # fn main() -> io::Result<()> {
+    /// let plugin = Plugin::load_file("Morrowind.esm")?;
+    /// assert_eq!(plugin.author(), "Bethesda Softworks");
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn author(&self) -> &str {
         &self.author
     }
 
+    /// Sets the plugin author string
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`PluginError::LimitExceeded`] if the length of the string exceeds [`AUTHOR_LENGTH`].
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use tesutil::plugin::*;
+    ///
+    /// # fn main() -> Result<(), PluginError> {
+    /// let mut plugin = Plugin::new(String::from("wrong author"), String::from("some description"))?;
+    /// plugin.set_author(String::from("correct author"))?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`PluginError::LimitExceeded`]: enum.PluginError.html#variant.LimitExceeded
+    /// [`AUTHOR_LENGTH`]: constant.AUTHOR_LENGTH.html
     pub fn set_author(&mut self, author: String) -> Result<(), PluginError> {
         check_size(&author, AUTHOR_LENGTH, "author value too long")?;
         self.author = author;
         Ok(())
     }
 
+    /// Returns the plugin description string
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use tesutil::plugin::*;
+    /// # use std::io;
+    ///
+    /// # fn main() -> io::Result<()> {
+    /// let plugin = Plugin::load_file("Bloodmoon.esm")?;
+    /// assert_eq!(plugin.description(), "The main data file for BloodMoon.\r\n(requires Morrowind.esm to run)");
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn description(&self) -> &str {
         &self.description
     }
 
+    /// Sets the plugin description string
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`PluginError::LimitExceeded`] if the length of the string exceeds [`DESCRIPTION_LENGTH`].
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use tesutil::plugin::*;
+    ///
+    /// # fn main() -> Result<(), PluginError> {
+    /// let mut plugin = Plugin::new(String::from("author"), String::from("some description"))?;
+    /// plugin.set_description(String::from("updated description"))?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`PluginError::LimitExceeded`]: enum.PluginError.html#variant.LimitExceeded
+    /// [`DESCRIPTION_LENGTH`]: constant.AUTHOR_LENGTH.html
     pub fn set_description(&mut self, description: String) -> Result<(), PluginError> {
         check_size(&description, DESCRIPTION_LENGTH, "descriptioin value too long")?;
         self.description = description;
         Ok(())
     }
 
+    /// Adds a new master to this plugin
+    ///
+    /// Masters are other plugins that this plugin depends on. `name` should be the filename of the
+    /// master file (without path, including extension) and `size` should be the size in bytes of
+    /// the master file (this is used by the game to check whether a master has changed since the
+    /// last time this plugin was updated).
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`PluginError::DuplicateMaster`] if `name` is already in the list of masters
+    /// (case insensitive).
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use tesutil::plugin::*;
+    ///
+    /// # fn main() -> Result<(), PluginError> {
+    /// let mut plugin = Plugin::new(String::from("author"), String::from("some description"))?;
+    /// plugin.add_master(String::from("Morrowind.esm"), 79837557)?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`PluginError::DuplicateMaster`]: enum.PluginError.html#variant.DuplicateMaster
     pub fn add_master(&mut self, name: String, size: u64) -> Result<(), PluginError> {
         // don't add it if it's already in the list
-        if !self.masters.iter().any(|m| m.0 == name) {
+        // TODO: find out if doing a case-insensitive comparison is appropriate for OpenMW running
+        //  on Linux
+        if !self.masters.iter().any(|m| m.0.to_lowercase() == name.to_lowercase()) {
             self.masters.push((name, size));
             Ok(())
         } else {
@@ -277,6 +438,36 @@ impl Plugin {
         }
     }
 
+    /// Adds a new record to this plugin
+    ///
+    /// Note: you should not explicitly add a TES3/TES4 record; this will be added automatically
+    /// based on the metadata from the `Plugin` struct.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`PluginError::LimitExceeded`] if adding the record would result in the plugin
+    /// having more than [`MAX_RECORDS`] records (extremely unlikely). Returns a
+    /// [`PluginError::DuplicateId`] if the record has the same ID as an existing record in this
+    /// plugin.
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// use tesutil::plugin::*;
+    ///
+    /// # fn main() -> Result<(), PluginError> {
+    /// let mut plugin = Plugin::new(String::from("test"), String::from("sample plugin"))?;
+    /// let mut record = Record::new(b"GMST");
+    /// record.add_field(Field::new_string(b"NAME", String::from("iDispKilling"))?);
+    /// record.add_field(Field::new_i32(b"INTV", -50));
+    /// plugin.add_record(record)?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`PluginError::LimitExceeded`]: enum.PluginError.html#variant.LimitExceeded
+    /// [`MAX_RECORDS`]: constant.MAX_RECORDS.html
+    /// [`PluginError::DuplicateId`]: enum.PluginError.html#variant.DuplicateId
     pub fn add_record(&mut self, record: Record) -> Result<(), PluginError> {
         check_size(&self.records, MAX_RECORDS, "too many records")?;
 
@@ -293,14 +484,76 @@ impl Plugin {
         Ok(())
     }
 
+    /// Finds a record by ID
+    ///
+    /// If no record exists with the given ID, the return value will be `None`.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use tesutil::plugin::*;
+    /// # use std::io;
+    ///
+    /// # fn main() -> io::Result<()> {
+    /// let plugin = Plugin::load_file("Morrowind.esm")?;
+    /// if let Some(record) = plugin.get_record("HortatorVotes") {
+    ///     // do something with record
+    /// }
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn get_record(&self, id: &str) -> Option<Ref<Record>> {
         self.id_map.get(id).map(|r| r.borrow())
     }
 
+    /// Finds a record by ID and returns a mutable reference
+    ///
+    /// If no record exists with the given ID, the return value will be `None`.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use tesutil::plugin::*;
+    /// # use std::io;
+    ///
+    /// # fn main() -> io::Result<()> {
+    /// let plugin = Plugin::load_file("Morrowind.esm")?;
+    /// if let Some(mut record) = plugin.get_record_mut("HortatorVotes") {
+    ///     // change something with record
+    /// }
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn get_record_mut(&self, id: &str) -> Option<RefMut<Record>> {
         self.id_map.get(id).map(|r| r.borrow_mut())
     }
 
+    /// Writes a plugin to the provided writer
+    ///
+    /// Writes a plugin to any type that implements [`Write`] or a mutable reference to such a type.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`std::io::Error`] if an I/O error occurs or if the plugin data is invalid. Plugin
+    /// data being invalid includes situations like record data being too large or string values
+    /// containing internal null bytes.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use tesutil::plugin::*;
+    ///
+    /// # fn main() -> std::io::Result<()> {
+    /// let mut buf: Vec<u8> = vec![];
+    /// let plugin = Plugin::load_file("Morrowind.esm")?;
+    /// plugin.write(&mut &mut buf)?;
+    /// assert_eq!(buf.len(), 79837557);
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`Write`]: https://doc.rust-lang.org/std/io/trait.Write.html
+    /// [`std::io::Error`]: https://doc.rust-lang.org/std/io/struct.Error.html
     pub fn write<T: Write>(&self, mut f: T) -> io::Result<()> {
         let mut header = Record::new(b"TES3");
         let mut buf: Vec<u8> = Vec::with_capacity(HEADER_LENGTH);
@@ -329,6 +582,30 @@ impl Plugin {
         Ok(())
     }
 
+    /// Save a plugin to a file
+    ///
+    /// The file must not exist.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`std::io::Error`] if the file cannot be created or if [`Plugin::write`] fails;
+    /// refer to that method for more information.
+    ///
+    /// # Examples
+    ///
+    /// ```no_run
+    /// use tesutil::plugin::*;
+    ///
+    /// # fn main() -> Result<(), Box<dyn std::error::Error>> {
+    /// let mut plugin = Plugin::new(String::from("test"), String::from("sample plugin"))?;
+    /// plugin.is_master = true;
+    /// plugin.save_file("sample.esm")?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    ///
+    /// [`std::io::Error`]: https://doc.rust-lang.org/std/io/struct.Error.html
+    /// [`Plugin::write`]: #method.write
     pub fn save_file(&self, path: &str) -> io::Result<()> {
         let f = File::create(path)?;
         let mut writer = BufWriter::new(f);
